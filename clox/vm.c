de185980f0ee23e4b386cb411b3967cfa5d053ca
Ivan Sadikov <ivan.sadikov@gmail.com>
2020-04-19 16:03:06 +0200

    add value types

--- clox/vm.c
+++ clox/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,6 +12,20 @@ VM vm; // static vm instance. TODO: pass a pointer instead
 
 static void resetStack() {
   vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int line = getLine(&vm.chunk->lines, instruction);
+  fprintf(stderr, "[line %d] in script\n", line);
+
+  resetStack();
 }
 
 void initVM() {
@@ -35,15 +50,28 @@ Value pop() {
   return *vm.stackTop;
 }
 
+Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
+}
+
+static bool isFalsey(Value value) {
+  // Follows Ruby in that nil and false are falsey and every other value behaves like true
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_CONSTANT_LONG() (vm.chunk->constants.values[READ_BYTE() | (READ_BYTE() << 8) | (READ_BYTE() << 16) | (READ_BYTE() << 24)])
-#define BINARY_OP(op) \
+#define BINARY_OP(valueType, op) \
   do { \
-    double b = pop(); \
-    double a = pop(); \
-    push(a op b); \
+    if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+      runtimeError("Operands must be numbers."); \
+      return INTERPRET_RUNTIME_ERROR; \
+    } \
+    double b = AS_NUMBER(pop()); \
+    double a = AS_NUMBER(pop()); \
+    push(valueType(a op b)); \
   } while (false)
 
 #ifdef DEBUG_TRACE_EXECUTION
@@ -73,24 +101,30 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_NIL: push(NIL_VAL); break;
+      case OP_TRUE: push(BOOL_VAL(true)); break;
+      case OP_FALSE: push(BOOL_VAL(false)); break;
+      case OP_EQUAL: {
+        Value a = pop();
+        Value b = pop();
+        push(BOOL_VAL(valuesEqual(a, b)));
+        break;
+      }
+      case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
+      case OP_LESS: BINARY_OP(BOOL_VAL, <); break;
+      case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
+      case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+      case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+      case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /); break;
+      case OP_NOT:
+        push(BOOL_VAL(isFalsey(pop())));
+        break;
       case OP_NEGATE: {
-        push(-pop());
-        break;
-      }
-      case OP_ADD: {
-        BINARY_OP(+);
-        break;
-      }
-      case OP_SUBTRACT: {
-        BINARY_OP(-);
-        break;
-      }
-      case OP_MULTIPLY: {
-        BINARY_OP(*);
-        break;
-      }
-      case OP_DIVIDE: {
-        BINARY_OP(/);
+        if (!IS_NUMBER(peek(0))) {
+          runtimeError("Operand must be a number");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        push(NUMBER_VAL(-AS_NUMBER(pop())));
         break;
       }
       case OP_RETURN: {
